15d2f5070013b94e3e2dd45ca1742e3c6526e50c
termite9 <ttermite0@gmail.com>
2023-12-11 15:37:14 +0900

    수정

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from posts.models import Post, PostImage, Comment
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo",
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+    ]
+
